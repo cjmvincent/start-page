46b11bd39d331d9c269f00fd492f4fb0fcd90008
Charles Vincent <cjmvincent@outlook.com>
2026-07-18 19:14:12 -0400

    seperate bookmarks to their own file

---
+++ bookmarks.js
@@ -0,0 +1,88 @@
+/**
+ * Bookmarks data
+ * Edit this file to add/remove/rename bookmark groups and links.
+ * `scripts.js` reads the `bookmarks` variable defined here.
+ */
+
+const bookmarks = [
+  {
+    label: "~/tech",
+    bookmarks: [
+      {
+        label: "r/unixporn",
+        url: "https://www.reddit.com/r/unixporn/"
+      },
+      {
+        label: "r/mechanicalkeyboards",
+        url: "https://www.reddit.com/r/mehanicalkeyboards/"
+      },
+      {
+        label: "r/workspaces",
+        url: "https://www.reddit.com/r/workspaces/"
+      }
+    ]
+  },
+  {
+    label: "~/work",
+    bookmarks: [
+      {
+        label: "classlink",
+        url: "https://launchpad.classlink.com/bryan"
+      },
+      {
+        label: "r/sysadmin",
+        url: "https://www.reddit.com/r/sysadmin/"
+      },
+      {
+        label: "r/k12sysadmin",
+        url: "https://www.reddit.com/r/k12sysadmin/"
+      }
+    ]
+  },
+  {
+    label: "~/dev",
+    bookmarks: [
+      {
+        label: "github",
+        url: "https://github.com/"
+      },
+      {
+        label: "ansible",
+        url: "https://docs.ansible.com/ansible/latest/cli/ansible-doc.html"
+      },
+      {
+        label: "pypi",
+        url: "https://pypi.org/"
+      },
+      {
+        label: "brew",
+        url: "https://brew.sh/"
+      },
+      {
+        label: "winget",
+        url: "https://winget.run/"
+      }
+    ]
+  },
+  {
+    label: "~/school",
+    bookmarks: [
+      {
+        label: "brightspace",
+        url: "https://kennesaw.view.usg.edu/"
+      },
+      {
+        label: "owlexpress",
+        url: "https://owlexpress.kennesaw.edu/"
+      },
+      {
+        label: "ksumail",
+        url: "https://outlook.office.com/"
+      },
+      {
+        label: "degreeworks",
+        url: "https://degreeworks.kennesaw.edu/"
+      }
+    ]
+  }
+]
